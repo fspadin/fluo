12c6356e107aabe5a36f1ca8a87fa027afe650f4
ACh <anna.chmielinska@uj.edu.pl>
2017-09-16 00:32:30 +0200

    Touch-ups to meet pylint cryteria.
     On branch master
     Changes to be committed:
    	modified:   examples/lifetime/fit_biexp_cstat.py
    
     Changes not staged for commit:
    	modified:   examples/lifetime/fit_biexp_iteratively.py
    	modified:   examples/lifetime/fit_biexp_to_multiple_data_chi2stat.py
    	modified:   fluo/fitter.py

--- examples/lifetime/fit_biexp_cstat.py
+++ examples/lifetime/fit_biexp_cstat.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-"""
-Example of fitting a convolved bi-exponential fluorescence decay to a single measurement using C Statistic.
-"""
-
+# -*- coding: utf-8 -*-
+"""Example of fitting a convolved bi-exponential fluorescence decay to
+a single measurement using C Statistic."""
+from fluo.fitter import make_lifetime_fitter
+from matplotlib import pyplot as plt
 import numpy as np
 np.set_printoptions(threshold=np.nan)
-import matplotlib
-from matplotlib import pyplot as plt
-from fluo.fitter import make_lifetime_fitter
 
-def main():  
+def main():
     file = np.loadtxt('../decay_2exp_1ns_02_4ns_08.txt', skiprows=1)
-    time, irf, decay = file[:, 0], file[:, 1], file[:, 2]    
+    time, irf, decay = file[:, 0], file[:, 1], file[:, 2]
     model_kwargs_e2 = {
         'model_components': 2,
         'model_parameters': {
@@ -21,34 +19,33 @@ def main():
             'tau1': {'value': 1, 'vary': True, 'min': 1E-6},
             'tau2': {'value': 5, 'vary': True, 'min': 1E-6},
             'offset': {'value': 0.1, 'vary': True, 'min': 1E-6},
-            'shift': {'value': 1, 'vary': True}      
+            'shift': {'value': 1, 'vary': True}
         },
         'fit_start': 2,
         'fit_stop': None
-    }   
+    }
     # Convolution fit with Chi2 Statistic
     cstat_fitter = \
     make_lifetime_fitter(
-        model_kwargs_e2, 
-        time, 
-        decay, 
+        model_kwargs_e2,
+        time,
+        decay,
         instrument_response=irf,
         fit_statistic='c_statistic'
         )
     cstat_fit = cstat_fitter.fit(report=True)
     # plot
     plt.plot(time, decay, 'bo', label='decay')
-    plt.plot(time, irf, 'go', label='irf')    
+    plt.plot(time, irf, 'go', label='irf')
     plt.plot(
         cstat_fitter.independent_var['time'],
-        cstat_fit.best_fit, 
-        'r-', 
-            label='fit')
-    plt.legend(loc='best')            
+        cstat_fit.best_fit,
+        'r-',
+        label='fit')
+    plt.legend(loc='best')
     plt.yscale('log')
-    plt.show()    
+    plt.show()
 
 
 if __name__ == "__main__":
     main()
-
