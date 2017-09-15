7c0512f42873d2fedfb05ffbacb7c46e7d9a00e7
ACh <anna.chmielinska@uj.edu.pl>
2017-09-16 01:07:26 +0200

    Touch-ups to meet pylint cryteria.
     On branch master
     Changes to be committed:
    	modified:   examples/lifetime/fit_biexp_iteratively.py
    
     Changes not staged for commit:
    	modified:   examples/lifetime/fit_biexp_to_multiple_data_chi2stat.py
    	modified:   examples/lifetime/fit_monoexp_Cstat.py
    	modified:   examples/lifetime/fit_monoexp_chi2stat.py
    	modified:   examples/lifetime/fit_monoexp_iteratively.py
    	modified:   examples/lifetime/tail_fit_monoexp_Cstat.py
    	modified:   examples/lifetime/tail_fit_monoexp_chi2stat.py
    	modified:   fluo/fitter.py

--- examples/lifetime/fit_biexp_iteratively.py
+++ examples/lifetime/fit_biexp_iteratively.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python3
 
-"""
-Module with example of fitting lifetimes to single measurements of fluorescence decay using iterative least squares. This method should elimate the bias introduced by fitting using Gaussian distribution. For comparison the fit using C Statistic is included.
-"""
+# -*- coding: utf-8 -*-
+"""Iterative least squares.
 
-import numpy as np
-np.set_printoptions(threshold=np.nan)
-import matplotlib
-from matplotlib import pyplot as plt
-cmap = plt.get_cmap('gist_rainbow')
+Module with example of fitting lifetimes to single measurements of
+fluorescence decay using iterative least squares. This method should elimate
+the bias introduced by fitting using Gaussian distribution. For a comparison
+the fit using C Statistic is included.
+"""
 from fluo.fitter import make_lifetime_fitter, iterative_least_squares
+from matplotlib import pyplot as plt
+import numpy as np
+CMAP = plt.get_cmap('gist_rainbow')
+np.set_printoptions(threshold=np.nan)
 
-def main():    
+def main():
     file = np.loadtxt('../decay_2exp_1ns_02_4ns_08.txt', skiprows=1)
-    time, irf, decay = file[:, 0], file[:, 1], file[:, 2]    
+    time, irf, decay = file[:, 0], file[:, 1], file[:, 2]
     model_kwargs_e2 = {
         'model_components': 2,
         'model_parameters': {
             'amplitude1': {'value': 0.5, 'vary': True, 'min': 1E-6},
             'amplitude2': {'value': 0.5, 'vary': True, 'min': 1E-6},
             'tau1': {'value': 1, 'vary': True, 'min': 1E-6},
-            'tau2': {'value': 5, 'vary': True, 'min': 1E-6},            
+            'tau2': {'value': 5, 'vary': True, 'min': 1E-6},
             'offset': {'value': 0.1, 'vary': True},
-            'shift': {'value': 0.5, 'vary': True}                  
+            'shift': {'value': 1, 'vary': True}
         },
         'fit_start': 2,
         'fit_stop': None
-    }    
-    
+    }
     # ConvolutionFitter C Statistic for comparison
     cstat_fitter = \
     make_lifetime_fitter(
-        model_kwargs_e2, 
-        time, 
-        decay, 
-        instrument_response=irf, 
+        model_kwargs_e2,
+        time,
+        decay,
+        instrument_response=irf,
         fit_statistic='c_statistic'
         )
     cstat_fit = cstat_fitter.fit()
-    
     # ConvolutionFitter Chi2 Statistic for initialization
     chi2stat_fitter = \
     make_lifetime_fitter(
-        model_kwargs_e2, 
-        time, 
-        decay, 
-        instrument_response=irf, 
+        model_kwargs_e2,
+        time,
+        decay,
+        instrument_response=irf,
         fit_statistic='chi_square_statistic'
         )
-      
     n_iter = 20
     fits = iterative_least_squares(chi2stat_fitter, n_iter)
     # plot
     plt.plot(time, decay, 'bo', label='decay')
-    plt.plot(time, irf, 'go', label='irf')   
+    plt.plot(time, irf, 'go', label='irf')
     plt.plot(
         cstat_fitter.independent_var['time'],
         cstat_fit.best_fit,
@@ -63,9 +63,9 @@ def main():
     for ith, fit in enumerate(fits):
         plt.plot(
             chi2stat_fitter.independent_var['time'],
-            fit.best_fit, 
-            label='{}-ith iter'.format(ith), 
-            color=cmap(ith / n_iter)
+            fit.best_fit,
+            label='{}-ith iter'.format(ith),
+            color=CMAP(ith / n_iter)
             )
     plt.legend(loc='best')
     plt.yscale('log')
